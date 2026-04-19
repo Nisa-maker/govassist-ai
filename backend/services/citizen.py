3b26580516b8708cc253eeb3b1274c8caaab1c20
Nisa-maker <nannisamsaek@gmail.com>
2026-04-19 12:39:12 +0700

    commit

--- backend/services/citizen.py
+++ backend/services/citizen.py
@@ -1,9 +1,9 @@
 from backend.services.population import get_population
-from services.education import get_education
-from services.health import get_health
-from services.employment import get_employment
-from backend.services.economy import get_economi
-from services.social import get_social
+from backend.services.education import get_education
+from backend.services.health import get_health
+from backend.services.employment import get_employment
+from backend.services.economy import get_economy
+from backend.services.social import get_social_assistance
 
 def get_citizen_data(citizen_id):
     return {
@@ -12,6 +12,6 @@ def get_citizen_data(citizen_id):
         **get_education(citizen_id),
         **get_health(citizen_id),
         **get_employment(citizen_id),
-        **get_economi(citizen_id),
+        **get_economy(citizen_id),
         **get_social(citizen_id),
     }
